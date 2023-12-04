7fbdb3a1de92e77062b7f9a5f9a07885adb41999
aquova <mail@aquova.net>
2023-12-04 10:02:06 -0600

    Improved day 4

--- day4/day4.nim
+++ day4/day4.nim
@@ -1,21 +1,22 @@
-import math, sequtils, sets, strutils, sugar
+import math, sequtils, strutils, sugar
 
 proc getNumMatches(line: string): int =
     let sections = line.split({':', '|'})
     let winners = collect:
         for x in sections[1].splitWhitespace():
-            {parseInt(x)}
-    let have = collect:
+            parseInt(x)
+    let matches = collect:
         for x in sections[2].splitWhitespace():
-            {parseInt(x)}
-    let matches = winners.intersection(have)
+            let ix = parseInt(x)
+            if ix in winners: ix
     return matches.len()
 
 proc day4p1*(input: string): string =
     var total = 0
     for line in input.splitLines():
         let matches = line.getNumMatches()
-        total += (if matches == 0: 0 else: 1 shl (matches - 1))
+        if matches != 0:
+            total += 1 shl (matches - 1)
     return $total
 
 proc day4p2*(input: string): string =
@@ -23,8 +24,7 @@ proc day4p2*(input: string): string =
     var cards = repeat(1, lines.len())
     for idx, line in lines.pairs():
         let matches = line.getNumMatches()
-        if matches == 0:
-            continue
+        if matches == 0: continue
         for dx in countup(1, matches):
             if idx + dx < cards.len():
                 cards[idx + dx] += cards[idx]
